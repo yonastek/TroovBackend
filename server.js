2ed0a2f5effa862f0ed98c9a8d27c6fe119efa42
yonas <teklemariamyonas@gmail.com>
2022-11-10 00:18:21 +0100

    =added controllers , middleware and routes

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 
+// Route files
+const objets = require("./routes/objets");
+
+//import logger
+const logger = require("pino-http")();
 const app = express();
+//app.use(logger);
+//mount routers
+app.use("/api/v1/objects", objets);
 
 const PORT = process.env.PORT || 5000;
 
