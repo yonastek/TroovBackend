8e0809f34cdaf7ebf79f2b3c78f20db27e12387b
yonas <teklemariamyonas@gmail.com>
2022-11-09 21:17:28 +0100

    initial express setup

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
